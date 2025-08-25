797e9e8262ebe2bda73b8c5fed74e6242272590d
RamonsArchive <rmcdeem.m@gmail.com>
2025-08-24 22:00:10 -0700

    incread lets connect size

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -14,7 +14,7 @@ const ContactButton = ({
       href="/contact"
       id="contact-button"
       ref={ref}
-      className={`contact-gradient-btn px-4 py-2 rounded-lg cursor-pointer text-center w-full max-w-md font-funnel-sans text-white text-[12px] xs:text-[14px] font-medium z-[50] invisible ${className}`}
+      className={`contact-gradient-btn px-4 py-2 rounded-lg cursor-pointer text-center w-full max-w-md font-funnel-sans text-white text-[12px] xs:text-[14px] md:text-[16px] font-medium z-[50] invisible ${className}`}
     >
       Let&apos;s Connect
     </Link>
